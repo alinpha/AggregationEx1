24892b0032872d7d36e1bd06721a7435914cace7
Aline Vetrov <alinpha@gmail.com>
2020-03-12 17:24:17 -0300

    Student class created

---
+++ AggregateEx1/ClassLib/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLib
+{
+    public class Student
+    {
+        public string Address { get; set; }
+
+        public float GPA { get; set; }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public Student() { }
+
+        override public string ToString()
+        {
+            return $"Name: {Name}\nAddress: {Address}\nID: {Id}\nGPA: {GPA}\n";
+        }
+    }
+}
